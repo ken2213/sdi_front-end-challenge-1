2f458c0b7164b5aa20421f92c5c88a7bbac3be45
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-05-20 14:56:34 +0800

    Created: PaginationSection component layout

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { 
+    Pagination, 
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious
+} from './ui/pagination'
+
+const PaginationSection = () => {
+    return (
+        <section className='bg-green-500 absolute w-full bottom-0'>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">2</PaginationLink>
+                    </PaginationItem>
+                    
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">8</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">9</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">10</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem className=' bg-toyota-red text-white'>
+                        <PaginationLink href="#">11</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">12</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">13</PaginationLink>
+                    </PaginationItem>
+                    
+                    <PaginationItem>
+                        <PaginationLink href="#">14</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink href="#">56</PaginationLink>
+                    </PaginationItem>
+                    
+                    <PaginationItem>
+                        <PaginationLink href="#">57</PaginationLink>
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+
+        </section>
+    )
+}
+
+export default PaginationSection
