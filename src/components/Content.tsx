92106245f92ed5938ceea0e0809c20e2945ae383
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-05-20 14:55:36 +0800

    Created: Content component layout

---
+++ src/components/Content.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <section>
+        <p className='text-toyota-red'>Darwin Tumaneng</p>
+        <h1 className='font-bold text-gray-800 text-[2rem]'>
+            Toyota Mobility Solutions PH Empowers Seafood wholesaler Mida Food with digital logistics platform
+        </h1>
+        <p>Logistics Platform address limitations in Mida Food's delivery operations/</p>
+        <p><a href="">Read Article</a></p>
+    </section>
+  )
+}
+
+export default Content
