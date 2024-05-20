93e6e62b69aeed01583a18db1f6c5c626dea1650
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-05-20 14:54:09 +0800

    Added: toyota-red color property

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        "toyota-red": "#eb0a1e",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
