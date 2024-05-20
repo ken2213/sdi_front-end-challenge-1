742d1b413e68f8c8ba873dd42cee5cef6b7bb3be
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-05-20 21:26:41 +0800

    Implemented: Overall responsiveness of webpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import PaginationSection from "@/components/Pagination";
 
 export default function Home() {
   return (
-    <main className="">
-      <Hero />
-      <hr className="w-full h-[3px] bg-gray-500" />
-      <Content />
-      <PaginationSection />
+    <main className="w-full h-screen flex justify-center">
+      <div className="w-full sm:w-[98%] md:w-[97%] lg:w-[80%] xl:w-[1100px]">
+          <Hero />
+          <hr className="w-full h-[3px] bg-gray-500 my-1" />
+          <Content />
+          <PaginationSection />
+      </div>
     </main>
   );
 }
