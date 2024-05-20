31465196fb44b849ad9e94dedf09ab0453750cdd
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-05-20 21:27:30 +0800

    Improved: UI and UX of Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import { Button } from './ui/button'
-import { CornerUpRight } from 'lucide-react'
+import { CornerUpRight, TriangleRight } from 'lucide-react'
 
 const Hero = () => {
   return (
-    <section className='bg-red-500 min-h-[400px] '>
-        <div className='bg-orange-500 h-[450px]'>
+    <section className=' min-h-[400px] '>
+        <div className='bg-orange-500 max-h-[500px] flex justify-center'>
+          <img 
+            src='./tmsph-ls-midafood.jpeg' 
+            className='w-full object-cover'
+          />
+        </div>
+
+        <div className='absolute z-10 h-[80px] w-[80px] top-[48%] ml-8 '>
+          {/* Parallelogram */}
+          <div className='skew-x-[-12] bg-toyota-red  w-full h-full'>
+            <TriangleRight className='absolute bottom-1 right-1 text-white fill-white' size={15} />
+          </div>
+
+          <div className='absolute top-0 w-full h-full pt-2 text-white'>
+            <div className=' flex justify-center items-center  w-full h-full'>
+              <div className='flex flex-col gap-2'>
+                <h1 className='text-[1.8rem] text-center leading-3 font-semibold'>13</h1>
+                <p className='text-center text-[0.8rem]'>JAN</p>
+              </div>
+            </div>
+          </div>
+        </div>
         
-        </div>
-        <div className='absolute bg-yellow-500 z-50 h-[80px] w-[80px] top-[43%] ml-8'>
-          <h1>13</h1>
-          <p>JAN</p>
-        </div>
-        <div className='flex justify-end h-[80px]'>
-          <Button>
-            <CornerUpRight /> &nbsp; Share
+        <div className='flex justify-end items-center h-[80px] pr-8'>
+          <Button variant="ghost" className='font-bold'>
+            <CornerUpRight /> &nbsp; SHARE
           </Button>
         </div>
     </section>
