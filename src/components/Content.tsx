1fc283c5b7c570e437d95d5e645a89f82d71df32
Kenneth <40328234+ken2213@users.noreply.github.com>
2024-05-20 21:28:08 +0800

    Improved: UI & UX of Content section

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,12 +3,22 @@ import React from 'react'
 const Content = () => {
   return (
     <section>
+      <div>
         <p className='text-toyota-red'>Darwin Tumaneng</p>
-        <h1 className='font-bold text-gray-800 text-[2rem]'>
-            Toyota Mobility Solutions PH Empowers Seafood wholesaler Mida Food with digital logistics platform
+      </div>
+      <div>
+        <h1 className='font-bold text-gray-800 text-[2rem] leading-9'>
+          Toyota Mobility Solutions PH Empowers Seafood wholesaler Mida Food with digital logistics platform
         </h1>
-        <p>Logistics Platform address limitations in Mida Food's delivery operations/</p>
-        <p><a href="">Read Article</a></p>
+        <p className='py-5'>
+          Logistics Platform address limitations in Mida Food's delivery operations
+        </p>
+      </div>
+        <p>
+          <a href="" className=' underline font-bold'>
+            Read Article
+          </a>
+        </p>
     </section>
   )
 }
